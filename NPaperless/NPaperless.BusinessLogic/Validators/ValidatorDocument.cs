f75d7d745214a7901244f40aaeb42f15689604f1
M.Porubsky <porubsky1995@hotmail.com>
2023-11-23 13:45:43 +0100

    preliminary validators

---
+++ NPaperless/NPaperless.BusinessLogic/Validators/ValidatorDocument.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using NPaperless.BusinessLogic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPaperless.BusinessLogic.Validators
+{
+    internal class ValidatorDocument : AbstractValidator<Document>
+    {
+        public ValidatorDocument()
+        {
+            RuleSet("RequiredData", () =>
+            {
+                RuleFor(DocumentType => DocumentType.Title).NotEmpty().NotNull().WithMessage("Title required");
+                RuleFor(DocumentType => DocumentType.UploadDocument).NotEmpty().NotNull().WithMessage("Document required");
+            });
+        }
+    }
+}
