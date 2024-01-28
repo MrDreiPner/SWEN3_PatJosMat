800116732d680a3871307fd156f69bcebf49df17
M.Porubsky <porubsky1995@hotmail.com>
2024-01-28 14:09:52 +0100

    panic push

--- NPaperless/NPaperless.REST/Controllers/SearchApi.cs
+++ NPaperless/NPaperless.REST/Controllers/SearchApi.cs
@@ -22,6 +22,7 @@ using NPaperless.REST;
 using log4net;
 using NPaperless.BusinessLogic.Services;
 using NPaperless.BusinessLogic.Interfaces;
+using System.IO.Pipelines;
 
 namespace NPaperless.REST.Controllers
 { 
@@ -59,7 +60,12 @@ namespace NPaperless.REST.Controllers
             {
                 stringResult.Add(doc.Title + ": " + doc.Content);
             }
-            return new ObjectResult(stringResult);
+            string realResult = null;
+            if (stringResult.Count > 0)
+            {
+                realResult = stringResult[0];
+            }
+            return new ObjectResult(realResult);
             //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
             // return StatusCode(200, default(List<string>));
             //string exampleJson = null;
